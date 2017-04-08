0e694ea8161029e16209931bd82c40fc790e59ac
Brian Barrow <briancbarrow@gmail.com>
2017-04-07 21:32:23 -0600

    finished with the video

--- app/index.js
+++ app/index.js
@@ -1,2 +1,14 @@
-<!DOCTYPE html>
-<
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Main = React.createClass({
+  render() {
+    return (
+      <p>Hello World!</p>
+    )
+  }
+})
+
+ReactDOM.render(
+  <Main />, document.getElementById('app')
+)
